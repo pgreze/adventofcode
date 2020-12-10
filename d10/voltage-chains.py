7e6cf42e4da8362d42e1dbef129519a8487cd926
pgreze <pierrick.greze@gmail.com>
2020-12-10 18:42:44 +0900

    Add day 10 solution 2 attempt 2 :(

--- d10/voltage-chains.py
+++ d10/voltage-chains.py
@@ -6,17 +6,21 @@ from collections import Counter
 def main(input_file):
     lines = open(input_file).readlines()
     numbers = list(set(int(l.strip()) for l in lines))
-    chains = list(create_adapter_chains(numbers, 0))
-    print(f"Available chains = {len(chains)}")
+    count = pow(2, adapter_chain_count(numbers))
+    print(f"Available chains = {count}")
 
-def create_adapter_chains(numbers: list, number: int, chain = []):
-    chain = chain + [number]
-    if number == numbers[-1]:
-        yield chain
-
-    for step in range(1, 4):
-        if number + step in numbers:
-            yield from create_adapter_chains(numbers, number + step, chain)
+def adapter_chain_count(numbers: list, index = 0):
+    if index >= len(numbers):
+        return 0
+    count, number = 0, numbers[index]
+    chain = [number]
+    while (index + count + 1) < len(numbers) and \
+        numbers[index + count + 1] == (numbers[index + count] + 1):
+        print(f"{number} + {numbers[index + count + 1]}")
+        chain += [numbers[index + count + 1]]
+        count += 1
+    if count: print(f"{count - 1} with {chain}")
+    return max(0, count - 1) + adapter_chain_count(numbers, index + count + 1)
 
 if __name__ == '__main__':
     main(sys.argv[1])
