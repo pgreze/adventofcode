9090c905fcb390e8f09fff1e92cc2d02c32430d3
pgreze <pierrick.greze@gmail.com>
2020-12-10 17:59:16 +0900

    Add day 10 solution

---
+++ d10/voltage-nightmare.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import Counter
+
+def main(input_file):
+    lines = open(input_file).readlines()
+    numbers = list(set(int(l.strip()) for l in lines))
+    diffs = count_diffs(numbers)
+    d1 = diffs[1] + 1
+    d3 = diffs[3] + 1
+    print(f"{d1} * {d3} = {d1 * d3}")
+
+def count_diffs(numbers: list):
+    return Counter(
+        numbers[idx + 1] - i
+        for idx, i in enumerate(numbers[:-1])
+    )
+
+if __name__ == '__main__':
+    main(sys.argv[1])
